ee3b76a94e78b02a9e75d972f398be4f371382b2
Mehmet <mehmeticme42@gmail.com>
2020-10-24 12:52:54 -0500

    Java Annotations - @Value

---
+++ Spring-05-ValueInjection/src/main/java/com/practice/MyApp.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import com.practice.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course = (Course) container.getBean("java");
+        System.out.println(course.toString());
+    }
+}
---
+++ Spring-05-ValueInjection/src/main/java/com/practice/services/Java.java
@@ -0,0 +1,46 @@
+package com.practice.services;
+
+import com.practice.interfaces.Course;
+import com.practice.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+    @Value("${student}")
+    private String studentName;
+
+    @Value("${days}")
+    private String[] days;
+
+    @Value("String Value")
+    private String testString;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "studentName='" + studentName + '\'' +
+                ", days=" + Arrays.toString(days) +
+                ", testString='" + testString + '\'' +
+                '}';
+    }
+
+    private ExtraSessions extraSessions1;
+    private ExtraSessions extraSessions2;
+
+    @Autowired
+    public Java(@Qualifier("officeHours") ExtraSessions extraSessions1, @Qualifier("reviewHours") ExtraSessions extraSessions2) {
+        this.extraSessions1 = extraSessions1;
+        this.extraSessions2 = extraSessions2;
+    }
+
+
+    @Override
+    public void getLearningHours() {
+        System.out.println("Weekly learning hours: 20 + Office Hours: " + extraSessions1.getHours() + " + Review Hours : "+extraSessions2.getHours());
+    }
+}
